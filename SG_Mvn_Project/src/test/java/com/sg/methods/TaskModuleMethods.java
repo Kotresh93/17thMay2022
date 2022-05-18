94481080931ac26ffd62922472e4fd27d17c4f4a
unknown <you@example.com>
2022-05-18 18:19:28 +0530

    Revert "taskModuleMethod modified by adding method name"
    This reverts commit 8b9eb6979a3d236ae630c1d14b5b8fe89c7b35a9.

--- SG_Mvn_Project/src/test/java/com/sg/methods/TaskModuleMethods.java
+++ SG_Mvn_Project/src/test/java/com/sg/methods/TaskModuleMethods.java
@@ -3,15 +3,5 @@ package com.sg.methods;
 import com.sg.driver.DriverScript;
 
 public class TaskModuleMethods extends DriverScript{
-	
-	/**************************************
-	 * Method Name : navigateURL 
-	 * Purpose : To navigate the URL 
-	 * Author : 
-	 * Reviewer Name: 
-	 * Date Written : 
-	 * Return value : boolean 
-	 * Param : oDriver, URL
-	 **************************************/
 
 }
